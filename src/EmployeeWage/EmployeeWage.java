14f1e8a55347815943785e4b8d684e905bcac3bb
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-16 18:46:11 +0530

    Added: Employee Attendance

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -3,5 +3,12 @@ package EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage");
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_FULL_TIME)
+            System.out.println("Empolyee is present");
+        else
+            System.out.println("Empolyee is absent");
     }
 }
+
