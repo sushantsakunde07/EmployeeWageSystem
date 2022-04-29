fd668b209518417ea0636f8f5b4ade02fb042cb5
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-29 08:01:00 +0530

    Added: Saved total wages for companies

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -1,36 +1,59 @@
 package EmployeeWage;
 
 public class EmployeeWage {
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUMBER_OF_WORKING_DAYS = 20;
-    public static final int MAXIMUM_HOURS_IN_MONTH = 100;
+    public static final int isFullTime = 1;
+    public static final int isPartTime = 2;
+    private final String company;
+    private final int maximumHoursInMonth;
+    private final int numberOfWorkingDays;
+    private final int empRatePerHour;
+    private int totalEmployeeWage;
 
-    public static void main(String[] args) {
-        int employeeHour = 0;
+
+    public EmployeeWage(String company, int empRatePerHour, int numberOfWorkingDays, int maximumHoursInMonth) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.maximumHoursInMonth = maximumHoursInMonth;
+        this.numberOfWorkingDays = numberOfWorkingDays;
+    }
+
+    public void emplyeeDailyWage() {
+        int employeehour = 0;
+        int totalworkingdays = 0;
         int totalEmployeeHours = 0;
-        int totalWorkingDays = 0;
-        while (totalEmployeeHours <= MAXIMUM_HOURS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
-            totalWorkingDays++;
+        while (totalEmployeeHours < maximumHoursInMonth && totalworkingdays < numberOfWorkingDays) {
+            totalworkingdays++;
             double empcheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empcheck) {
-                case FULL_TIME:
-                    employeeHour = 8;
+                case isFullTime:
+                    employeehour = 8;
                     break;
-                case PART_TIME:
-                    employeeHour = 4;
+                case isPartTime:
+                    employeehour = 4;
                     break;
                 default:
-                    employeeHour = 0;
+                    employeehour = 0;
             }
-            totalEmployeeHours += employeeHour;
-            System.out.println("day :" + totalWorkingDays + " employee hour:" + employeeHour);
+            if (totalEmployeeHours + employeehour <= maximumHoursInMonth) {
+                totalEmployeeHours += employeehour;
+            }
+            //System.out.println("days : " + totalworkingdays + "employee hour:" + employeehour);
         }
-        int Totalemployeewage = totalEmployeeHours * EMP_RATE_PER_HOUR;
-        System.out.println("Total empolyee wage:" + Totalemployeewage);
-        System.out.println("Total empolyee hour:" + totalEmployeeHours);
-        System.out.println("Total empolyee working days:" + totalWorkingDays);
+        totalEmployeeWage = totalEmployeeHours * empRatePerHour;
+    }
+    @Override
+    public String toString() {
+        return "\n Total Employee Wage for " + company + " is: " + totalEmployeeWage ;
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage computation");
+        EmployeeWage wallmart = new EmployeeWage("Wallmart", 30, 65, 200);
+        wallmart.emplyeeDailyWage();
+        System.out.println(wallmart);
+        EmployeeWage reliance = new EmployeeWage("Reliance", 20, 20, 100);
+        reliance.emplyeeDailyWage();
+        System.out.println(reliance);
     }
 }
+
